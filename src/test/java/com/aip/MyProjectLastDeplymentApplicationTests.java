85babde13113ac99e890ee01a6cc53a7d157b1ea
RathnayakeEGB <rathnayakaegb@gmail.com>
2021-03-27 21:07:49 +0530

    'first-commit'

---
+++ src/test/java/com/aip/MyProjectLastDeplymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectLastDeplymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
